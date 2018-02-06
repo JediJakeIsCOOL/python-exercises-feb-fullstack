7c75fa84b90a4cc2b0811df11423341412f48bd2
Jacob Anderson <jedijake@Jacobs-MBP.localdomain>
2018-02-06 10:25:17 -0600

    exercise 1 num 3&4

---
+++ exercisepy2.py
@@ -0,0 +1,9 @@
+#3
+name2 = input("What is your name?")
+subj = input("What is your favorite subject in school?")
+print(name2 +"'s", "favorite subject in school is", subj)
+
+#4
+days = {"0":"Sunday", "1":"Monday", "2":"Tuesday","3":"Wednesday", "4": "Thursday", "5":"Friday"}
+whatday= input("what day is it 0-6?")
+print(days[whatday])
