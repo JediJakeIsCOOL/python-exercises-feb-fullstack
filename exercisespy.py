8ef807ad99aa30cb9f977ee13c7b9d9a583bd986
Jacob Anderson <jedijake@Jacobs-MBP.localdomain>
2018-02-06 10:20:51 -0600

    first exercises

---
+++ exercisespy.py
@@ -0,0 +1,6 @@
+#1
+name = input("What is your name?")
+print("Hello," + name)
+#2
+name = input("What is your name?")
+print("Hello," , name.upper() , "your name has" , len(name) , "in it! AWESOME!")
